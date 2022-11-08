3c1e7c053ea9122da6e4f134ee5a2c4f42addf39
kyontae harvey <79426140+dntsaveher@users.noreply.github.com>
2022-11-08 12:30:01 -0500

    Add files via upload

---
+++ example1.js
@@ -0,0 +1,15 @@
+let val = 0;
+
+
+function clean(val) {
+  document.getElementById('screen').value = ""
+  val = ""
+  return val
+}
+
+function display(val) {
+document.getElementById('screen').value+= val
+
+return val
+}
+
